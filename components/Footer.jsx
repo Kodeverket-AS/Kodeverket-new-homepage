15b040c6fea8b451fafd6a7eee7af1a7a93566d2
Monika <maam092024@gmail.com>
2025-03-05 16:51:34 +0100

    footer, services

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,21 +1,22 @@
 export default function Footer() {
   return (
-    <footer style=
-      {{
+    <footer
+      style={{
         backgroundImage: "url('/footerbg.jpeg')",
         backgroundSize: "cover",
         backgroundPosition: "center",
-      }} className="flex justify-between p-4 h-full ">
-      
-      <div>
-        <h3>Kontakt</h3>
+      }}
+      className="flex justify-between items-center p-4 h-96 "
+    >
+      <div className="flex flex-col  text-white mx-24">
+        <h3 className="text-xl font-bold pb-4">Kontakt</h3>
         <p>info@breipost.no</p>
         <p>9999 99 999</p>
       </div>
-      <div>
-        <h3>Snartvei</h3>
-        <li>Om oss</li>
-        <li>Tjenester</li>
+      <div className="flex flex-col  text-white mx-24 ">
+        <h3 className="text-xl font-bold pb-4">Snarvei</h3>
+        <a href="">Om oss</a>
+        <a href=""> Tjenester</a>
       </div>
     </footer>
   );
--- components/Services.jsx
+++ components/Services.jsx
@@ -1,7 +1,82 @@
-export default function Services() {
+import Card from "@mui/material/Card";
+import CardActions from "@mui/material/CardActions";
+import CardContent from "@mui/material/CardContent";
+import Typography from "@mui/material/Typography";
+
+
+ 
+export default function BasicCard() {
+   
   return (
-    <div>
-      <h1> her  kommer services</h1>
-    </div>
+    <>
+      <div className="text-center text-2xl font-bold py-12">
+        <h2> Hva gjør vi ? </h2>
+      </div>
+
+      <div className="flex flex-wrap justify-center gap-4">
+        <Card
+          className="flex justify-center items-center"
+          sx={{ minWidth: 275, maxWidth: 300 }}
+        >
+          <CardContent className="flex justify-center items-center">
+            <img src="/logocolor.jpeg" alt="breiflabb logo" />
+          </CardContent>
+        </Card>
+
+        <Card sx={{ minWidth: 275, maxWidth: 300 }}>
+          <CardContent>
+            <Typography
+              gutterBottom
+              sx={{ color: "text.secondary", fontSize: 14, marginBottom: 7.5 }}
+            >
+              01
+            </Typography>
+            <Typography sx={{ color: "text.secondary", mb: 1.5 }}>
+              UI/UX Design
+            </Typography>
+            <Typography variant="body2">
+              Vi skaper moderne, estetisk tiltalende og funksjonelle
+              designløsninger som gir en unik brukeropplevelse.
+            </Typography>
+          </CardContent>
+        </Card>
+
+        <Card sx={{ minWidth: 275, maxWidth: 300 }}>
+          <CardContent>
+            <Typography
+              gutterBottom
+              sx={{ color: "text.secondary", fontSize: 14, marginBottom: 7.5 }}
+            >
+              02
+            </Typography>
+            <Typography sx={{ color: "text.secondary", mb: 1.5 }}>
+              Front-end
+            </Typography>
+            <Typography variant="body2">
+              Vårt team utvikler intuitive og raske grensesnitt for
+              nettapplikasjoner som oppfyller de høyeste ytelsesstandardene.
+            </Typography>
+          </CardContent>
+        </Card>
+
+        <Card sx={{ minWidth: 275, maxWidth: 300 }}>
+          <CardContent>
+            <Typography
+              gutterBottom
+              sx={{ color: "text.secondary", fontSize: 14, marginBottom: 7.5 }}
+            >
+              03
+            </Typography>
+            <Typography sx={{ color: "text.secondary", mb: 1.5 }}>
+              Back-end
+            </Typography>
+            <Typography variant="body2">
+              Vi bygger pålitelige og skalerbare backend-systemer som sikrer
+              stabil drift og sikkerhet for nettapplikasjoner.
+            </Typography>
+          </CardContent>
+        </Card>
+      </div>
+    </>
   );
 }
