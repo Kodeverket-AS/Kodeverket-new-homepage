5e8a09ef5f0906069f499f96185bf5aeb240e18f
Monika <maam092024@gmail.com>
2025-03-05 12:03:37 +0100

    footer

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,7 +1,22 @@
 export default function Footer() {
   return (
-    <div>
-      <h1> her kommer Footer</h1>
-    </div>
+    <footer style=
+      {{
+        backgroundImage: "url('/footerbg.jpeg')",
+        backgroundSize: "cover",
+        backgroundPosition: "center",
+      }} className="flex justify-between p-4 h-full ">
+      
+      <div>
+        <h3>Kontakt</h3>
+        <p>info@breipost.no</p>
+        <p>9999 99 999</p>
+      </div>
+      <div>
+        <h3>Snartvei</h3>
+        <li>Om oss</li>
+        <li>Tjenester</li>
+      </div>
+    </footer>
   );
 }
