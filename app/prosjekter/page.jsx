36b0242609734ce01dcc85c8afcfa7ff8df78199
Andreastak86 <andreastak.kodehode@gmail.com>
2025-03-18 15:27:48 +0100

    testing and crossing fingers

--- app/prosjekter/page.jsx
+++ app/prosjekter/page.jsx
@@ -4,17 +4,17 @@ import ProsjektertIntro from "@/components/ProsjekterIntro";
 import Prosjketer from "@/components/Prosjekter";
 
 export default function KontaktPage() {
- return (
-   <div className="flex flex-col flex-flow ">
-     <NavBar />
+    return (
+        <div className='flex flex-col flex-flow '>
+            <NavBar />
 
-     <div className="flex flex-col flex-flow flex-grow-1 h-[100vh]">
-       <ProsjektertIntro />
-       <Prosjketer />
-    
-     </div>
+            <div className='flex flex-col flex-flow flex-grow-1 h-[100vh]'>
+                <ProsjektertIntro />
+                <Prosjketer />
+                <h1>Just a test....</h1>
+            </div>
 
-     <Footer />
-   </div>
- );
+            <Footer />
+        </div>
+    );
 }
