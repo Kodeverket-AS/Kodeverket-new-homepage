82e4a388a92d8bb6032e69635b9eccf599392b67
Stein-B06 <Steinb06.kodehode@gmail.com>
2025-03-19 13:11:47 +0100

    second commit carousel, added css

--- components/Prosjekter.jsx
+++ components/Prosjekter.jsx
@@ -1,5 +1,5 @@
 "use client";
-
+import "../app/globals.css";
 import { Swiper, SwiperSlide } from "swiper/react";
 import { Navigation, Pagination, EffectCoverflow } from "swiper/modules";
 import "swiper/css";
@@ -48,9 +48,9 @@ const slides = [
 
 export default function Carousel() {
   return (
-    <div className="bg-cyan-950 h- md:h-[85lvh] my-12 p-6 flex items-center justify-center">
+    <div className="bg-cyan-950 h-full my-8 p-6 my:mt-6 md:pb-10   flex items-center justify-center">
       <div className="w-full max-w-5xl relative ">
-        <h1 className="text-center my-10 font-semibold text-2xl md:text-4xl text-white">
+        <h1 className="text-center my-6   md:my-10 font-semibold text-2xl md:text-4xl text-white">
           Our projects
         </h1>
         <Swiper
@@ -76,17 +76,17 @@ export default function Carousel() {
               },
             },
           }}
-          className="relative"
+          className="relative max-w-lg  md:min-w-full"
         >
           {slides.map((slide, index) => (
             <SwiperSlide key={index} className="relative overflow-hidden group">
-              <div className="w-full h-110 flex items-center justify-center sm:bg-transparent">
+              <div className="w-full h-72 md:h-85 flex items-center justify-center sm:bg-transparent">
                 <Image
                   src={slide.src}
                   alt={slide.title}
                   width={600}
                   height={400}
-                  className="w-full h-80 object-cover duration-700  sm:group-[.swiper-slide-active]:scale-120"
+                  className="w-full h-80 object-cover duration-700  sm:group-[.swiper-slide-active]:scale-120 select-none"
                 />
               </div>
               <div className="absolute inset-0  bg-sky-700  flex items-center justify-center flex-col text-white p-4 transition-opacity opacity-100 duration-700 sm:opacity-0 sm:group-[.swiper-slide-active]:opacity-90">
@@ -94,16 +94,16 @@ export default function Carousel() {
                   <h1 className="text-2xl mb-6 font-bold">{slide.title}</h1>
                   <p className="text-sm mb-4">{slide.description}</p>
                 </div>
-                <button className="bg-cyan-950 border-1 border-white text-white px-6 py-2 mt-10 rounded hover:bg-cyan-800 transition ">
+                <button className="text-sm md:text-base bg-cyan-950 border-1 border-white text-white px-6 py-2 mt-10 rounded hover:bg-cyan-800 transition ">
                   {slide.cta}
                 </button>
               </div>
             </SwiperSlide>
           ))}
+          <div className="swiper-button-prev  cursor-pointer "></div>
+          <div className="swiper-button-next cursor-pointer "></div>
         </Swiper>
         {/* Custom Navigation Buttons */}
-        <div className="swiper-button-prev absolute -left-10 top-1/2 transform  z-10 text-white text-3xl cursor-pointer hidden sm:block"></div>
-        <div className="swiper-button-next absolute right-0 top-1/2 transform  z-10 text-white text-3xl cursor-pointer hidden sm:block"></div>
       </div>
     </div>
   );
