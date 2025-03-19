2061c554efe82688d0f5a933047889fe3c1564fa
Monika <maam092024@gmail.com>
2025-03-19 12:15:21 +0100

    oppdatering plus den nye karouselen

--- components/KontaktIntro.jsx
+++ components/KontaktIntro.jsx
@@ -6,7 +6,7 @@ export default function KontaktIntro() {
         backgroundImage: "url('/ContactUs.png')",
       }}
     >
-      <div className="flex flex-col p-4 items-center gap-4 max-w-2/3 lg:max-w-1/3 mx-auto  bg-white/30 border-white border backdrop-blur-md text-cyan-950 mb-8">
+      <div className="flex flex-col p-4 items-center gap-4 max-w-2/3 lg:max-w-4/5 mx-auto  bg-white/30 border-white border backdrop-blur-md text-cyan-950 mb-8">
         <h1 className="text-3xl md:text-5xl font-bold mt-2">Kontakt oss</h1>
         <p className="text-md text-center">
           Har du spørsmål eller trenger mer informasjon? Ikke nøl med å ta
--- components/TjenesterIntro.jsx
+++ components/TjenesterIntro.jsx
@@ -2,14 +2,14 @@
 export default function TjenesterIntro() {
   return (
     <div
-      className="flex flex-col justify-end w-full h-[50vh] md:h-[40vh] max-h-[50vh] bg-cover md:bg-contain bg-center bg-no-repeat"
+      className="flex flex-col justify-end w-full h-[100vh] md:h-[40vh] max-h-[50vh] bg-cover md:bg-contain bg-center bg-no-repeat "
       style={{
         backgroundImage: "url('/Service.png')",
       }}
     >
-      <div className="flex flex-col p-4 items-center gap-4 max-w-2/3 lg:max-w-1/3 mx-auto bg-white/30 border-white border backdrop-blur-xl text-cyan-950 mb-8">
+      <div className="flex flex-col p-4 items-center gap-4 max-w-2/3 lg:max-w-4/5 mx-auto bg-white/30 border-white border backdrop-blur-xl text-cyan-950 mb-8">
         <h1 className="text-3xl md:text-5xl font-bold mt-2">Tjenester</h1>
-        <p className="text-md text-center xl:text-xl">
+        <p className="text-md text-center 5xl:text-xl">
           Vi tilbyr tjenester som webutvikling, mobilapplikasjonsutvikling og
           tilpassede IT-løsninger. Vi leverer også skreddersydde løsninger for
           bedrifter, inkludert design, testing og vedlikehold av digitale
