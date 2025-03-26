1597afad0250aa0bb3f20bb3e8b440bbfe1f8b75
vegardgkoder <vegardgkoder@gmail.com>
2025-03-26 13:00:06 +0100

    validation checks added

--- sanity/schemaTypes/project.js
+++ sanity/schemaTypes/project.js
@@ -8,28 +8,33 @@ export default {
       title: "Logo",
       type: "image",
       options: { hotspot: true },
+      validation: Rule => Rule.required().error('logo is required'),
     },
     {
       name: "background",
       title: "background",
       type: "image",
       options: { hotspot: true },
+      validation: Rule => Rule.required().error('background is required'),
     },
     {
       name: "companyName",
       title: "Company Name",
       type: "string",
+      validation: Rule => Rule.required().error('company name is required'),
     },
     
     {
       name: "url",
       title: "Project URL",
       type: "url",
+      validation: Rule => Rule.required().error('url to project'),
     },
     {
       name: "description",
       title: "Description",
       type: "text",
+      validation: Rule => Rule.max(400).warning('Description must be less then 500 characters'),
     },
     {
       name: "technologies",
