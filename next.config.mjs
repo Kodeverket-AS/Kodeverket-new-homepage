744b88a9ac7937a618c76705ee24ab443fa67eb1
Monika <maam092024@gmail.com>
2025-03-27 16:01:16 +0100

    update sanity configuration

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   env: {
-    SANITY_PROJECT_ID: "dm8vyhbj",
+    SANITY_PROJECT_ID: "6ci6a7t9",
     SANITY_DATASET: "production",
   },
   images: {
@@ -9,8 +9,9 @@ const nextConfig = {
       {
         protocol: "https",
         hostname: "cdn.sanity.io",
-       
-        pathname: "/**", },
+
+        pathname: "/**",
+      },
     ],
   },
 };
