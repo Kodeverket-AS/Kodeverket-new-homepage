ae48dbc91a1b16e430d1aa6fb3ab55ec4151f658
Andreastak86 <andreastak.kodehode@gmail.com>
2025-04-23 14:22:45 +0200

    think its fixed now

--- components/History.jsx
+++ components/History.jsx
@@ -1,34 +1,43 @@
 export default function History() {
-  return (
-    <div>
-      <div className="flex flex-col  mx-4 md:mx-18">
-        <h1 className="text-2xl lg:text-4xl font-bold text-center py-6">
-          {" "}
-          Vår historie
-        </h1>
-        <div className="flex flex-col-reverse md:flex-row">
-          <div className="md:w-4/9 text-base flex flex-col items-left justify-center gap-4 py-4">
-            <p>
-              {" "}
-              Det startet Oktober 2022. Da var det fire kandidater fra
-              utviklerkurset, Kodehode som ble tilbudt praksisplass. Fra start,
-              skulle det utvikles et helt nytt prosjekt som senere ble til
-              Bergen Armwrestling sin nettside. I Kodeverket Bergen er vi
-              opptatt av at hver enkelt praksiskandidat får muligheten til å
-              utvikle seg faglig og personlig. Vi skal kunne snu oss fort rundt
-              og hjelpe våre kunder med det som trengs. Kodeverket Bergen er
-              opptatt av å levere nettsider som ser like bra ut både på PC og
-              mobil. Sammen med våre kunder setter vi oss alltid ned for å finne
-              den løsningen som passer deg best.
-            </p>{" "}
-          </div>
-          <img
-            src="/historie.png"
-            alt="man in fornt of computer"
-            className="md:w-5/9"
-          />
+    return (
+        <div>
+            <div className='flex flex-col  mx-4 md:mx-18'>
+                <h1 className='text-4xl font-bold text-center py-6'>
+                    {" "}
+                    Vår historie
+                </h1>
+                <div className='flex flex-col-reverse md:flex-row'>
+                    <div className='md:w-4/9 text-base flex flex-col items-left justify-center gap-4 py-4'>
+                        <p>
+                            {" "}
+                            Kodeverket Bergen ble grunnlagt i oktober 2022 med
+                            et mål om å skape en trygg arena for utvikling, både
+                            faglig og personlig. Oppstarten begynte med fire
+                            kandidater fra utviklerkurset Kodehode, som fikk
+                            praksisplass og muligheten til å være med på å forme
+                            et helt nytt prosjekt. Dette prosjektet utviklet seg
+                            til nettsiden for Bergen Armwrestling—et symbol på
+                            hva dedikasjon og samarbeid kan utrette. Siden den
+                            gang har Kodeverket Bergen fortsatt å vokse, med et
+                            sterkt fokus på å gi praksiskandidater sjansen til å
+                            oppnå sitt potensial, samtidig som vi leverer
+                            skreddersydde digitale løsninger til våre kunder. Vi
+                            jobber raskt og effektivt for å møte behovene til
+                            bedrifter, og alle nettsider vi utvikler ser like
+                            bra ut på både PC og mobil. Hos Kodeverket Bergen
+                            handler det om mer enn teknologi—det handler om
+                            mennesker. Sammen med våre kunder finner vi alltid
+                            frem til den perfekte løsningen, skreddersydd for
+                            akkurat deg.
+                        </p>{" "}
+                    </div>
+                    <img
+                        src='/historie.png'
+                        alt='man in fornt of computer'
+                        className='md:w-5/9'
+                    />
+                </div>
+            </div>
         </div>
-      </div>
-    </div>
-  );
+    );
 }
