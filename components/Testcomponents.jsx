8ebeac422ef388f0aecc9cfe6c0403046cb6c6e4
Andreastak86 <andreastak.kodehode@gmail.com>
2025-12-11 10:54:35 +0100

    feat: add Test component for initial layout and styling

---
+++ components/Testcomponents.jsx
@@ -0,0 +1,17 @@
+export default function Test() {
+    return (
+        <div
+            className='flex flex-col justify-end w-full h-[50vh] md:h-[40vh] max-h-[50vh]  bg-cover md:bg-contain bg-center bg-no-repeat'
+            style={{
+                backgroundImage: "url('/ContactUs.png')",
+            }}
+        >
+            <div className='flex flex-col p-4 items-center gap-4 max-w-2/3 lg:max-w-4/5 mx-auto  bg-white/30 border-white border backdrop-blur-md text-cyan-950 mb-8'>
+                <h1 className='text-3xl  font-bold mt-2'>Test oss</h1>
+                <p className='text-base text-center'>
+                    Dette er en test komponent.....
+                </p>
+            </div>
+        </div>
+    );
+}
